5ab9ccf338474659a487ef6bb0af797751222fe0
kaniini <kaniini@dereferenced.org>
2018-10-30 02:49:00 +0000

    kill more irc bot stuff

--- relay/__main__.py
+++ relay/__main__.py
@@ -3,7 +3,6 @@ import aiohttp.web
 import logging
 
 from . import app
-from .irc import irc_bot
 
 
 async def start_webserver():
@@ -19,7 +18,6 @@ async def start_webserver():
 def main():
     loop = asyncio.get_event_loop()
     asyncio.ensure_future(start_webserver())
-    asyncio.ensure_future(irc_bot())
     loop.run_forever()
 
 
