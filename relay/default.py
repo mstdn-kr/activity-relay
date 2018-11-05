d74fc15198ac272180d470534aafec21210f399f
tristan <gled@tristans-MacBook-Pro.local>
2018-11-04 18:41:08 -0800

    Revert "Revert "expose the list of registered targets to default page""
    This reverts commit 51d2ab4e36b03d274cea64fa97a307573a87326d.

--- relay/default.py
+++ relay/default.py
@@ -1,8 +1,10 @@
 import aiohttp.web
 from . import app, CONFIG
+from .database import DATABASE
 
 host = CONFIG['ap']['host']
 note = CONFIG['note']
+targets = '<br>'.join([target for target in DATABASE.get('relay-list', [])])
 
 async def default(request):
     return aiohttp.web.Response(
@@ -23,8 +25,9 @@ async def default(request):
 <p>For Mastodon instances, you may subscribe to this relay with the address: <a href="https://{host}/inbox">https://{host}/inbox</a></p>
 <p>For Pleroma and other instances, you may subscribe to this relay with the address: <a href="https://{host}/actor">https://{host}/actor</a></p>
 <p>To host your own relay, you may download the code at this address: <a href="https://git.pleroma.social/pleroma/relay">https://git.pleroma.social/pleroma/relay</a></p>
+<br><p>List of registered instances:<br>{targets}</p>
 </body></html>
 
-""".format(host=host, note=note))
+""".format(host=host, note=note,targets=targets))
 
 app.router.add_get('/', default)
