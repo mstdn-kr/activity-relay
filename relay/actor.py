44faf30162c06ad89660fe0b0ee5444aa1505da3
kaniini <nenolod@gmail.com>
2019-01-17 23:04:07 +0000

    Revert "Merge branch 'fix-html' into 'master'"
    This reverts merge request !8

--- relay/actor.py
+++ relay/actor.py
@@ -35,11 +35,7 @@ from . import app, CONFIG
 from .remote_actor import fetch_actor
 
 
-AP_CONFIG = CONFIG.get('ap', {
-    'host': 'localhost',
-    'blocked_instances': [],
-    'allowed_instances': [],
-})
+AP_CONFIG = CONFIG.get('ap', {'host': 'localhost','blocked_instances':[]})
 CACHE_SIZE = CONFIG.get('cache-size', 16384)
 
 
@@ -220,10 +216,6 @@ async def handle_follow(actor, data, request):
     inbox = get_actor_inbox(actor)
 
     if urlsplit(inbox).hostname in AP_CONFIG['blocked_instances']:
-        return
-
-    if AP_CONFIG['allowed_instances'] and\
-            urlsplit(inbox).hostname not in AP_CONFIG['allowed_instances']:
         return
 
     if inbox not in following:
