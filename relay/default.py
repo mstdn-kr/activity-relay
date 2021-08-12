0456e4ca5008ea534957f25ffd5024b37b798f2d
Sam Therapy <sam@samtherapy.net>
2021-08-12 20:13:10 +0000

    Add Misskey to the list of instances that can subscribe using /inbox

--- relay/default.py
+++ relay/default.py
@@ -25,7 +25,7 @@ async def default(request):
 <body>
 <p>This is an Activity Relay for fediverse instances.</p>
 <p>{note}</p>
-<p>For Mastodon instances, you may subscribe to this relay with the address: <a href="https://{host}/inbox">https://{host}/inbox</a></p>
+<p>For Mastodon and Misskey instances, you may subscribe to this relay with the address: <a href="https://{host}/inbox">https://{host}/inbox</a></p>
 <p>For Pleroma and other instances, you may subscribe to this relay with the address: <a href="https://{host}/actor">https://{host}/actor</a></p>
 <p>To host your own relay, you may download the code at this address: <a href="https://git.pleroma.social/pleroma/relay">https://git.pleroma.social/pleroma/relay</a></p>
 <br><p>List of {count} registered instances:<br>{targets}</p>
